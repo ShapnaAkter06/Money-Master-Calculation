826eb9e1db2ab85f428bde88a31479dd0fbbb0eb
Shapna <shapna088@gmail.com>
2022-08-21 17:38:35 +0600

    git position changed

--- js/app.js
+++ js/app.js
@@ -4,18 +4,18 @@ document.getElementById('calculate-button').addEventListener('click', function()
     const rentExpenses = getInputFieldValueById('rent-expenses');
     const clothExpenses = getInputFieldValueById('cloth-expenses');
 
-    if( (isNaN(totalIncome) || totalIncome < 0) || (isNaN(foodExpenses) || foodExpenses < 0) || (isNaN(rentExpenses) || rentExpenses < 0) || (isNaN(clothExpenses) || clothExpenses < 0) ){
+    if( (isNaN(foodExpenses) || foodExpenses < 0) || (isNaN(rentExpenses) || rentExpenses < 0) || (isNaN(clothExpenses) || clothExpenses < 0) ){
         alert("Please check your all input field");
         return;
     }
 
     const totalCost = foodExpenses + rentExpenses + clothExpenses;
-    
-    if(totalCost > totalIncome){
+    seTextElementValueById('total-expenses', totalCost);
+
+    if((isNaN(totalIncome) || totalIncome < 0) || (totalCost > totalIncome)){
         alert ("You can't spend money more than your income");
         return;
     }
-    seTextElementValueById('total-expenses', totalCost);
 
     const newbalance = totalIncome - totalCost;
     seTextElementValueById('balance', newbalance)
@@ -30,13 +30,13 @@ document.getElementById('save-button').addEventListener('click', function(){
         return;
     }
     const newSaveAmount = totalIncome * (saveInput / 100);
+    seTextElementValueById('save-amount', newSaveAmount);
 
     if (newbalance < newSaveAmount){
         alert("You can not save money more than your balance");
         return;
     }
-    seTextElementValueById('save-amount', newSaveAmount);
-
+    
     const currentBalance = newbalance - newSaveAmount;
     seTextElementValueById('remaining-balance', currentBalance)
 })
