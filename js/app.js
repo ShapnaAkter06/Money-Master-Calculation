9662f022ce4c09155e27863d55fcb04798f5f4f8
Shapna <shapna088@gmail.com>
2022-08-21 17:20:36 +0600

    code added

--- js/app.js
+++ js/app.js
@@ -6,6 +6,7 @@ document.getElementById('calculate-button').addEventListener('click', function()
 
     if( (isNaN(totalIncome) || totalIncome < 0) || (isNaN(foodExpenses) || foodExpenses < 0) || (isNaN(rentExpenses) || rentExpenses < 0) || (isNaN(clothExpenses) || clothExpenses < 0) ){
         alert("Please check your all input field");
+        return;
     }
 
     const totalCost = foodExpenses + rentExpenses + clothExpenses;
@@ -24,14 +25,14 @@ document.getElementById('save-button').addEventListener('click', function(){
     const saveInput = getInputFieldValueById('save-input');
     const newbalance = getTextElementValueById('balance');
 
-    if(isNaN(saveInput) || saveInput > 100 || saveInput < 0){
+    if(isNaN(saveInput) || saveInput > 100 || saveInput < 0 || isNaN(totalIncome)){
         alert("Please enter valid number");
-        return
+        return;
     }
     const newSaveAmount = totalIncome * (saveInput / 100);
 
     if (newbalance < newSaveAmount){
-        alert("You can not save money more than your income");
+        alert("You can not save money more than your balance");
         return;
     }
     seTextElementValueById('save-amount', newSaveAmount);
